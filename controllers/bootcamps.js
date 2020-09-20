c57cf54e2305813351c6efd9b7b8b89958c244c5
Cezary Bąk <cezarybak@MBP-Cezary.home>
2020-09-20 23:32:24 +0200

    Add sort&sort&pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,16 +4,59 @@ const ErrorRessponse = require("../utils/errorResponse");
 const geocoder = require("../utils/geocoder");
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;  
+  let query;
+  
+  const reqQuery =  {...req.query}; 
 
-  let queryStr = JSON.stringify(req.query);
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery);
 
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);  
+  
   query = Bootcamp.find(JSON.parse(queryStr)); 
+
+  if(req.query.select){
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields)
+  }
+
+  if(req.query.sort){
+    const sortyBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortyBy);
+  }else{
+    query = query.sort('-createdAt');
+  }
+
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query.skip(startIndex).limit(limit); 
+
+
   const bootcamp = await query;
+  const pagination = {};
+  if(endIndex < total){
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if(startIndex > 0){
+    pagination.prev = {
+      page: page -1,
+      limit
+    }
+  }
   res
     .status(200)
-    .json({ status: true, length: bootcamp.length, data: bootcamp });
+    .json({ status: true,pagination , length: bootcamp.length, data: bootcamp });
 });
 
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
